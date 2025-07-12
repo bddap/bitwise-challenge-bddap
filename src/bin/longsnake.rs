3205dc38e80c0b663478b4cb162f70a7350b56d8
Andrew Dirksen <andrew@dirksen.com>
2025-07-12 13:33:27 -0700

    enum repr for Turn and Direction

--- src/bin/longsnake.rs
+++ src/bin/longsnake.rs
@@ -1,6 +1,3 @@
-//! A modified version of https://github.com/zesterer/bitwise-examples/blob/main/examples/snake.rs
-//! It packs data more densely using non-integer numbers of bits for some elements.
-
 use std::{
     iter::once,
     ops::{Add, Neg},
@@ -9,48 +6,69 @@ use std::{
 use bitwise_challenge::{Game, Input, Key, Output};
 use bitwise_challenge_bddap::cheeky_encoding::{decode, encode};
 
-struct Snake;
-
 const CELLS: u32 = 8;
 const CELL: u32 = 32;
 const SCORE_H: u32 = 64;
 const SCORE_MAX: u8 = 19;
 const FIELD_COUNT: usize = 26;
 
+#[repr(i8)]
 #[derive(Copy, Clone, Debug, PartialEq)]
-struct Turn(i8);
-
-impl Turn {
-    const LEFT: Self = Turn(-1);
-    const STRAIGHT: Self = Turn(0);
-    const RIGHT: Self = Turn(1);
+enum Turn {
+    Left = -1,
+    Straight = 0,
+    Right = 1,
 }
 
-#[derive(PartialEq, Copy, Clone, Debug)]
-struct Direction(i8);
+impl From<i8> for Turn {
+    fn from(value: i8) -> Self {
+        let value = value.wrapping_add(1).rem_euclid(3) - 1;
+        match value {
+            -1 => Self::Left,
+            0 => Self::Straight,
+            1 => Self::Right,
+            _ => unreachable!(),
+        }
+    }
+}
+
+#[repr(i8)]
+#[derive(Copy, Clone, Debug, PartialEq)]
+enum Direction {
+    East = 0,
+    North = 1,
+    West = 2,
+    South = 3,
+}
 
 impl Direction {
-    const EAST: Self = Direction(0);
-    const NORTH: Self = Direction(1);
-    const WEST: Self = Direction(2);
-    const SOUTH: Self = Direction(3);
-
-    fn front(self) -> [i32; 2] {
-        match self.0 {
-            0 => [1, 0],
-            1 => [0, -1],
-            2 => [-1, 0],
-            3 => [0, 1],
+    fn from_int(value: i8) -> Self {
+        match value % 4 {
+            0 => Self::East,
+            1 => Self::North,
+            2 => Self::West,
+            3 => Self::South,
             _ => unreachable!(),
         }
     }
 
+    fn front(self) -> [i32; 2] {
+        match self {
+            Self::East => [1, 0],
+            Self::North => [0, -1],
+            Self::West => [-1, 0],
+            Self::South => [0, 1],
+        }
+    }
+
     fn relative(self, other: Self) -> Option<Turn> {
-        match (self.0 - other.0 + 4) % 4 {
-            0 => Some(Turn::STRAIGHT),
-            1 => Some(Turn::RIGHT),
+        let s = self as i8;
+        let o = other as i8;
+        match (s - o + 4) % 4 {
+            0 => Some(Turn::Straight),
+            1 => Some(Turn::Right),
             2 => None,
-            3 => Some(Turn::LEFT),
+            3 => Some(Turn::Left),
             _ => unreachable!(),
         }
     }
@@ -60,7 +78,7 @@ impl Neg for Direction {
     type Output = Self;
 
     fn neg(self) -> Self::Output {
-        Direction((self.0 + 2) % 4)
+        Self::from_int(self as i8 + 2)
     }
 }
 
@@ -68,9 +86,11 @@ impl Add<Turn> for Direction {
     type Output = Self;
 
     fn add(self, turn: Turn) -> Self::Output {
-        Self((self.0 + turn.0 + 4) % 4)
+        Self::from_int(self as i8 + turn as i8 + 4)
     }
 }
+
+struct Snake;
 
 struct Data {
     pos: [u32; 2],
@@ -91,29 +111,29 @@ impl Data {
         [
             self.pos[0] as u64,
             self.pos[1] as u64,
-            self.dir.0 as u64,
+            self.dir as i8 as u64,
             self.score as u64,
             self.fruit_pos[0] as u64,
             self.fruit_pos[1] as u64,
-            (self.tail[0].0 + 1) as u64,
-            (self.tail[1].0 + 1) as u64,
-            (self.tail[2].0 + 1) as u64,
-            (self.tail[3].0 + 1) as u64,
-            (self.tail[4].0 + 1) as u64,
-            (self.tail[5].0 + 1) as u64,
-            (self.tail[6].0 + 1) as u64,
-            (self.tail[7].0 + 1) as u64,
-            (self.tail[8].0 + 1) as u64,
-            (self.tail[9].0 + 1) as u64,
-            (self.tail[10].0 + 1) as u64,
-            (self.tail[11].0 + 1) as u64,
-            (self.tail[12].0 + 1) as u64,
-            (self.tail[13].0 + 1) as u64,
-            (self.tail[14].0 + 1) as u64,
-            (self.tail[15].0 + 1) as u64,
-            (self.tail[16].0 + 1) as u64,
-            (self.tail[17].0 + 1) as u64,
-            (self.tail[18].0 + 1) as u64,
+            (self.tail[0] as i8 + 1) as u64,
+            (self.tail[1] as i8 + 1) as u64,
+            (self.tail[2] as i8 + 1) as u64,
+            (self.tail[3] as i8 + 1) as u64,
+            (self.tail[4] as i8 + 1) as u64,
+            (self.tail[5] as i8 + 1) as u64,
+            (self.tail[6] as i8 + 1) as u64,
+            (self.tail[7] as i8 + 1) as u64,
+            (self.tail[8] as i8 + 1) as u64,
+            (self.tail[9] as i8 + 1) as u64,
+            (self.tail[10] as i8 + 1) as u64,
+            (self.tail[11] as i8 + 1) as u64,
+            (self.tail[12] as i8 + 1) as u64,
+            (self.tail[13] as i8 + 1) as u64,
+            (self.tail[14] as i8 + 1) as u64,
+            (self.tail[15] as i8 + 1) as u64,
+            (self.tail[16] as i8 + 1) as u64,
+            (self.tail[17] as i8 + 1) as u64,
+            (self.tail[18] as i8 + 1) as u64,
             self.is_dead as u64,
         ]
     }
@@ -150,29 +170,29 @@ impl Data {
     fn from_u64s(data: [u64; FIELD_COUNT]) -> Self {
         Self {
             pos: [data[0] as u32, data[1] as u32],
-            dir: Direction(data[2] as i8),
+            dir: Direction::from_int(data[2] as i8),
             score: data[3] as u8,
             fruit_pos: [data[4] as u32, data[5] as u32],
             tail: [
-                Turn(data[6] as i8 - 1),
-                Turn(data[7] as i8 - 1),
-                Turn(data[8] as i8 - 1),
-                Turn(data[9] as i8 - 1),
-                Turn(data[10] as i8 - 1),
-                Turn(data[11] as i8 - 1),
-                Turn(data[12] as i8 - 1),
-                Turn(data[13] as i8 - 1),
-                Turn(data[14] as i8 - 1),
-                Turn(data[15] as i8 - 1),
-                Turn(data[16] as i8 - 1),
-                Turn(data[17] as i8 - 1),
-                Turn(data[18] as i8 - 1),
-                Turn(data[19] as i8 - 1),
-                Turn(data[20] as i8 - 1),
-                Turn(data[21] as i8 - 1),
-                Turn(data[22] as i8 - 1),
-                Turn(data[23] as i8 - 1),
-                Turn(data[24] as i8 - 1),
+                (data[6] as i8 - 1).into(),
+                (data[7] as i8 - 1).into(),
+                (data[8] as i8 - 1).into(),
+                (data[9] as i8 - 1).into(),
+                (data[10] as i8 - 1).into(),
+                (data[11] as i8 - 1).into(),
+                (data[12] as i8 - 1).into(),
+                (data[13] as i8 - 1).into(),
+                (data[14] as i8 - 1).into(),
+                (data[15] as i8 - 1).into(),
+                (data[16] as i8 - 1).into(),
+                (data[17] as i8 - 1).into(),
+                (data[18] as i8 - 1).into(),
+                (data[19] as i8 - 1).into(),
+                (data[20] as i8 - 1).into(),
+                (data[21] as i8 - 1).into(),
+                (data[22] as i8 - 1).into(),
+                (data[23] as i8 - 1).into(),
+                (data[24] as i8 - 1).into(),
             ],
             is_dead: data[25] == 1,
         }
@@ -216,10 +236,10 @@ impl Game for Snake {
     fn init() -> u64 {
         make_state(Data {
             pos: [4, 4],
-            dir: Direction(0),
+            dir: Direction::East,
             score: 0,
             fruit_pos: [5, 3],
-            tail: [Turn(0); 19],
+            tail: [Turn::Straight; 19],
             is_dead: false,
         })
     }
@@ -255,23 +275,23 @@ impl Game for Snake {
             for i in (0..data.tail.len() - 1).rev() {
                 data.tail[i + 1] = data.tail[i];
             }
-            data.tail[0] = Turn::STRAIGHT;
+            data.tail[0] = Turn::Straight;
         }
 
         let new_dir = if input.is_key_down(Key::Right) {
-            Direction::EAST
+            Direction::East
         } else if input.is_key_down(Key::Left) {
-            Direction::WEST
+            Direction::West
         } else if input.is_key_down(Key::Up) {
-            Direction::NORTH
+            Direction::North
         } else if input.is_key_down(Key::Down) {
-            Direction::SOUTH
+            Direction::South
         } else {
             data.dir
         };
 
         if let Some(turn) = data.dir.relative(new_dir)
-            && turn != Turn::STRAIGHT
+            && turn != Turn::Straight
         {
             data.tail[0] = turn;
             data.dir = new_dir;
@@ -343,31 +363,29 @@ mod tests {
     #[test]
     fn parameterized_turns() {
         let test_cases = [
-            (Direction::EAST, Direction::NORTH, Some(Turn::LEFT)),
-            (Direction::NORTH, Direction::WEST, Some(Turn::LEFT)),
-            (Direction::WEST, Direction::SOUTH, Some(Turn::LEFT)),
-            (Direction::SOUTH, Direction::EAST, Some(Turn::LEFT)),
-            (Direction::EAST, Direction::SOUTH, Some(Turn::RIGHT)),
-            (Direction::NORTH, Direction::EAST, Some(Turn::RIGHT)),
-            (Direction::WEST, Direction::NORTH, Some(Turn::RIGHT)),
-            (Direction::SOUTH, Direction::WEST, Some(Turn::RIGHT)),
-            (Direction::EAST, Direction::EAST, Some(Turn::STRAIGHT)),
-            (Direction::NORTH, Direction::NORTH, Some(Turn::STRAIGHT)),
-            (Direction::WEST, Direction::WEST, Some(Turn::STRAIGHT)),
-            (Direction::SOUTH, Direction::SOUTH, Some(Turn::STRAIGHT)),
-            (Direction::EAST, Direction::WEST, None),
-            (Direction::NORTH, Direction::SOUTH, None),
-            (Direction::WEST, Direction::EAST, None),
-            (Direction::SOUTH, Direction::NORTH, None),
+            (Direction::East, Direction::North, Some(Turn::Left)),
+            (Direction::North, Direction::West, Some(Turn::Left)),
+            (Direction::West, Direction::South, Some(Turn::Left)),
+            (Direction::South, Direction::East, Some(Turn::Left)),
+            (Direction::East, Direction::South, Some(Turn::Right)),
+            (Direction::North, Direction::East, Some(Turn::Right)),
+            (Direction::West, Direction::North, Some(Turn::Right)),
+            (Direction::South, Direction::West, Some(Turn::Right)),
+            (Direction::East, Direction::East, Some(Turn::Straight)),
+            (Direction::North, Direction::North, Some(Turn::Straight)),
+            (Direction::West, Direction::West, Some(Turn::Straight)),
+            (Direction::South, Direction::South, Some(Turn::Straight)),
+            (Direction::East, Direction::West, None),
+            (Direction::North, Direction::South, None),
+            (Direction::West, Direction::East, None),
+            (Direction::South, Direction::North, None),
         ];
 
         for (from, to, expected) in test_cases {
             assert_eq!(
                 from.relative(to),
                 expected,
-                "Testing from {:?} to {:?}",
-                from,
-                to
+                "Testing from {from:?} to {to:?}",
             );
         }
     }
